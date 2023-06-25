4671a43396c682bb1bc3b782a3e8ca76e5ab8003
Rohit Katariya <rohitkori00@gmail.com>
2023-06-25 19:39:20 +0530

    remove duplicates 2

---
+++ src/RemoveDuplicates_80.java
@@ -0,0 +1,25 @@
+/*
+*
+*
+* Input: nums =     [1,1,1,2,2,3]
+* Output: 5, nums = [1,1,2,2,3,_]
+* Time = oN
+* space = o1
+* */
+
+import java.util.Arrays;
+
+public class RemoveDuplicates_80 {
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int n : nums)
+            if (i < 2 || n > nums[i - 2])
+                nums[i++] = n;
+
+        Arrays.stream(nums).forEach(System.out::println);
+        return i;
+    }
+    public static void main(String[] args) {
+        removeDuplicates(new int[]{1,1,1,2,2,3});
+    }
+}
