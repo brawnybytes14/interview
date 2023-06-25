690dd2a6ec7d93fd6f3c73aa1a74df1a93e0797d
Rohit Katariya <rohitkori00@gmail.com>
2023-06-25 20:02:51 +0530

    majority element

---
+++ src/MajorityElement_169.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class MajorityElement_169 {
+/*
+* Input: nums = [2,2,1,1,1,2,2]
+  Output: 2
+*
+*
+* */
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(majorityElement(new int[] {2,2,1,1,1,2,2}));
+    }
+}
